6a8d7f3c6972623702ae54dbafd58b676f923c57
Dharssiga Kathirvel <dharssigakathirvel@gmail.com>
2026-06-22 12:23:22 +0530

    fixed the files in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home            from "./pages/Home.js";
-import Opportunities   from "./pages/Opportunities.js";
-import Profile         from "./pages/Profile.js";
-import Recommendations from "./pages/Recommendations.js";
-import Signup          from "./pages/Signup.js";
-import AddScholarship  from "./pages/AddScholarship.js";
-import Dashboard       from "./pages/Dashboard.js";
-import Login           from "./pages/Login.js";
-import Games           from "./pages/Games.js";
+import Home            from "./pages/Home.jsx";
+import Opportunities   from "./pages/Opportunities.jsx";
+import Profile         from "./pages/Profile.jsx";
+import Recommendations from "./pages/Recommendations.jsx";
+import Signup          from "./pages/Signup.jsx";
+import AddScholarship  from "./pages/AddScholarship.jsx";
+import Dashboard       from "./pages/Dashboard.jsx";
+import Login           from "./pages/Login.jsx";
+import Games           from "./pages/Games.jsx";
 
 export default function App() {
   return (
